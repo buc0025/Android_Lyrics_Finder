2c5fb7398450545669c4c88a49e32887b658ec6a
buc0025 <stanley.yang001@gmail.com>
2020-10-21 15:35:42 -0400

    Updated app and added new activity
    Added new feature that allows user to search for artist and song on YouTube in a different activity.
    
    Also added new background image, and updated margins and fonts.

---
+++ LyricsFinder/app/src/main/java/com/example/lyricsfinder/Browse_youtube.java
@@ -0,0 +1,39 @@
+package com.example.lyricsfinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class Browse_youtube extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_browse_youtube);
+
+        final String path = "https://www.youtube.com/results?search_query=";
+
+        Intent intent = getIntent();
+        final String artist = intent.getStringExtra("Artist");
+        final String song = intent.getStringExtra("Song");
+
+        webView = findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(path + artist + "+" + song);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
--- LyricsFinder/app/src/main/java/com/example/lyricsfinder/MainActivity.java
+++ LyricsFinder/app/src/main/java/com/example/lyricsfinder/MainActivity.java
@@ -2,12 +2,12 @@ package com.example.lyricsfinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -19,14 +19,11 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.net.URL;
-
 public class MainActivity extends AppCompatActivity {
 
     EditText edtArtistName, edtSongName;
-    Button btnGetLyrics;
+    Button btnGetLyrics, btnYoutube;
     TextView txtLyrics;
-    //  https://api.lyrics.ovh/v1/Rihanna/Diamonds#
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,25 +33,42 @@ public class MainActivity extends AppCompatActivity {
         edtArtistName = findViewById(R.id.edtArtistName);
         edtSongName = findViewById(R.id.edtSongName);
         btnGetLyrics = findViewById(R.id.btnGetLyrics);
+        btnYoutube = findViewById(R.id.btnYoutube);
         txtLyrics = findViewById(R.id.txtLyrics);
 
+        btnYoutube.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String artist = edtArtistName.getText().toString();
+                String song = edtSongName.getText().toString();
+                song.replace(" ", "+");
+                Intent intent = new Intent(MainActivity.this, Browse_youtube.class);
+                intent.putExtra("Artist", artist);
+                intent.putExtra("Song", song);
+                startActivity(intent);
+            }
+        });
 
         btnGetLyrics.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-               // Toast.makeText(getApplicationContext(), "This button is tapped", Toast.LENGTH_SHORT).show();
-                String url = "https://api.lyrics.ovh/v1/" + edtArtistName.getText().toString() + "/" + edtSongName.getText().toString();
+                String url = "https://api.lyrics.ovh/v1/" + edtArtistName.getText().toString() + "/" +
+                        edtSongName.getText().toString();
                 url.replace(" ","%20");
-                RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
 
-                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                final RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url,
+                        null, new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
 
                         try {
-                            txtLyrics.setText(response.getString("lyrics"));
-
+                            if (response.getString("lyrics").length() != 0) {
+                                txtLyrics.setText(response.getString("lyrics"));
+                            } else {
+                                txtLyrics.setText("Song not found");
+                            }
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
@@ -63,19 +77,14 @@ public class MainActivity extends AppCompatActivity {
                         new Response.ErrorListener() {
                             @Override
                             public void onErrorResponse(VolleyError error) {
-
                                 if (error.networkResponse.statusCode==404) {
                                     txtLyrics.setText("Song not found");
                                 }
-
                             }
                         });
 
                 requestQueue.add(jsonObjectRequest);
-
             }
         });
-
-
     }
 }
